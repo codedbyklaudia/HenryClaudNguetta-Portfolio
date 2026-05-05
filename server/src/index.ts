5b9c4bb8b909d9c59aa75a4e43d69b3424b0cd3e
Klaudia <klaudia.kulawik98@outlook.com>
2026-05-05 21:04:05 +0100

    Changed the client url

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://henryclaudnguetta.co.uk/'
+  origin: process.env.CLIENT_URL || 'https://henryclaudnguetta.co.uk'
 }))
 app.use(express.json())
 app.use('/api', contactRoute)
